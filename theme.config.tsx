1134f46be17779addbffa09102cb7662320aa8c3
Khadim <khadimfall0602@gmail.com>
2024-04-14 20:26:12 +0200

    fix things :D

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,46 +1,67 @@
-import React from 'react'
-import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
-import { useRouter } from 'next/router'
+import { useState, useEffect } from "react";
+import { DocsThemeConfig, useConfig, useTheme } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <img src="/img/logo.png" className="h-8 dark:invert" />,
+  logo: () => {
+    const { theme } = useTheme();
+    const [darkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+      setIsDarkMode(theme === "dark");
+    }, [theme]);
+
+    return (
+      <img
+        src="/img/logo.png"
+        className={`h-8${darkMode ? " invert" : " invert-0"}`}
+      />
+    );
+  },
   project: {
-    link: 'https://github.com/PawnTown/docs.pawn.town.app',
+    link: "https://github.com/PawnTown/docs.pawn.town.app",
   },
   chat: {
-    link: 'https://discord.gg/SW7dgF3nH3',
+    link: "https://discord.gg/SW7dgF3nH3",
   },
-  docsRepositoryBase: 'https://github.com/PawnTown/docs.pawn.town.app/blob/main',
+  docsRepositoryBase:
+    "https://github.com/PawnTown/docs.pawn.town.app/blob/main",
   footer: {
-    text: '© 2023 PawnTown',
+    text: "© 2024 PawnTown",
   },
   useNextSeoProps() {
-    const { asPath } = useRouter()
-    if (asPath !== '/') {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
       return {
-        titleTemplate: '%s'
-      }
+        titleTemplate: "%s",
+      };
     }
   },
   head: () => {
-    const { asPath, defaultLocale, locale } = useRouter()
-    const { frontMatter } = useConfig()
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
     const url =
-      'https://docs.pawn.town' +
-      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
- 
+      "https://docs.pawn.town" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
     return (
       <>
         <meta property="og:url" content={url} />
-        <meta property="og:title" content={frontMatter.title || 'PawnTown Docs'} />
-        <meta property="og:img" content={frontMatter.image || ''} />
+        <meta
+          property="og:title"
+          content={frontMatter.title || "PawnTown Docs"}
+        />
+        <meta property="og:img" content={frontMatter.image || ""} />
         <meta
           property="og:description"
-          content={frontMatter.description || 'Official PawnTown Platform Documentation'}
+          content={
+            frontMatter.description ||
+            "Official PawnTown Platform Documentation"
+          }
         />
       </>
-    )
-  }
-}
+    );
+  },
+};
 
-export default config
+export default config;
